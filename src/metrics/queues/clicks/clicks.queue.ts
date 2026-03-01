c178761b6831968497e460919fea9193ec3f5632
Pedro Henrique Heing Geronimo <pedroheing@gmail.com>
2026-03-01 15:30:43 -0300

    refactor: changes geo and agent processing to worker

--- src/metrics/queues/clicks/clicks.queue.ts
+++ src/metrics/queues/clicks/clicks.queue.ts
@@ -1,11 +1,7 @@
-import { UserAgentResult } from 'src/common/decorators/user-agent.decorator';
-import { GeoInfo } from 'src/core/geolocation/geolocation.interface';
-
 export const CLICKS_QUEUE_KEY = 'metrics:clicks';
 
 export interface RegisterClickData {
 	shortUrlId: number;
 	ip: string;
-	geo: GeoInfo | null;
-	userAgent: UserAgentResult;
+	userAgent: string;
 }
